1b5dafc997c00ff04fc68a259d2b7ea9edfee919
Jonathan Mendes <jonathan_fortunato@hotmail.com>
2021-11-12 15:45:22 -0300

    decimo commit

---
+++ index.php
@@ -0,0 +1,200 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<title>Projeto 10</title>
+	<meta charset="utf-8">
+	<meta name="keywords" content="marketing digital,agencia,automoção de marketing">
+	<meta name="author" content="Jhonw Black">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0">
+</head>
+<body>
+	<header>
+		<div class="container">
+			<div class="menu-topo">
+			<div class="logo"></div>
+			<nav class="desktop">
+				<ul>
+					<li><a href="">Sobre</a></li>
+					<li><a href="">Serviços</a></li>
+					<li><a href="">Contato</a></li>
+					<li class="btn-nav"><a href="">Quero um orçamento</a></li>
+				</ul>
+			</nav>
+			<nav class="mobile">
+				<h3><i class="fa fa-bars"></i></h3>
+				<ul>
+					<li><a href="">Sobre</a></li>
+					<li><a href="">Serviços</a></li>
+					<li><a href="">Contato</a></li>
+					<li class="btn-nav"><a href="">Quero um orçamento</a></li>
+				</ul>
+			</nav>
+			<div class="clear"></div>
+		</div><!--menu-topo-->
+		<div class="chamada-header">
+			<h2>Você vendendo mais<br />com automação web.</h2>
+			<p>Nós criamos soluções de marketing para sua empresa vender mais, da melhor forma.</p>
+			<form>
+				<input type="email" required="" placeholder="Seu e-mail...">
+				<input type="submit" name="acao" value="Quero saber mais!">
+			</form>
+		</div><!--chamada-->
+		</div><!--container-->
+	</header>
+
+	<section class="descricao-danki">
+		<div class="container">
+		<p>Com anos de experiência em desenvolvimento web e automação de marketing, a <b>DankiCode</b> traz soluções que ajudam o seu time a vender mais tanto em segmentos B2C quanto B2B</p>
+	</div><!--container-->
+	</section>
+
+	<section class="descricao-danki2">
+		<div class="container">
+			<div class="texto-descricao">
+				<h3>Nós somos uma agência digital. Trabalhamos seu posicionamento, conteúdo e estética, sempre focado em conversão.</h3>
+				<p>Converse com a nossa equipe ainda hoje e crie um plano de marketing para o seu negócio!</p>
+			</div>
+			<img src="images/mockup1.jpg">
+			<div class="clear"></div>
+		</div>
+	</section>
+
+	<section class="descricao-jh3">
+		<div class="container">
+			<div class="box-depoimento">
+				<p>“Temos a Danki como parceiro estratégico nos nossos planejamento em plataforma digital. Sempre nos trazendo soluções inovadoras e adequadas às nossas necessidades e dos nossos clientes. Super recomendo!“</p>
+				<br/>
+				<p><b>Glaúcio - Voodoo Branded Content</b></p>
+			</div>
+		</div><!--container-->
+	</section>
+
+	<section class="servicos">
+		<div class="container">
+			<div class="title-servicos">
+				<h3>Como podemos ajudar sua empresas<br/><i class="fa fa-angle-down"></i></h3>
+			</div>
+			<div class="flex-servicos">
+				<div class="box-servicos">
+						<img src="images/img1.jpg">
+						<div class="box-servicos-wraper">
+						<h2>E-mail Marketing</h2>
+						<p>Aprenda como utilizar e-mail marketing na sua empresa para alavancar suas vendas de uma forma automatizada.</p>
+						<a href="#">Orçamento</a>
+					</div><!--box-wraper-->
+				</div><!--box-->
+
+				<div class="box-servicos">
+						<img src="images/img1.jpg">
+						<div class="box-servicos-wraper">
+						<h2>E-mail Marketing</h2>
+						<p>Aprenda como utilizar e-mail marketing na sua empresa para alavancar suas vendas de uma forma automatizada.</p>
+						<a href="#">Orçamento</a>
+					</div><!--box-wraper-->
+				</div><!--box-->
+
+				<div class="box-servicos">
+						<img src="images/img1.jpg">
+						<div class="box-servicos-wraper">
+						<h2>E-mail Marketing</h2>
+						<p>Aprenda como utilizar e-mail marketing na sua empresa para alavancar suas vendas de uma forma automatizada.</p>
+						<a href="#">Orçamento</a>
+					</div><!--box-wraper-->
+				</div><!--box-->
+
+				<div class="box-servicos">
+						<img src="images/img1.jpg">
+						<div class="box-servicos-wraper">
+						<h2>E-mail Marketing</h2>
+						<p>Aprenda como utilizar e-mail marketing na sua empresa para alavancar suas vendas de uma forma automatizada.</p>
+						<a href="#">Orçamento</a>
+					</div><!--box-wraper-->
+				</div><!--box-->
+
+				<div class="box-servicos">
+						<img src="images/img1.jpg">
+						<div class="box-servicos-wraper">
+						<h2>E-mail Marketing</h2>
+						<p>Aprenda como utilizar e-mail marketing na sua empresa para alavancar suas vendas de uma forma automatizada.</p>
+						<a href="#">Orçamento</a>
+					</div><!--box-wraper-->
+				</div><!--box-->
+
+				<div class="box-servicos">
+						<img src="images/img1.jpg">
+						<div class="box-servicos-wraper">
+						<h2>E-mail Marketing</h2>
+						<p>Aprenda como utilizar e-mail marketing na sua empresa para alavancar suas vendas de uma forma automatizada.</p>
+						<a href="#">Orçamento</a>
+					</div><!--box-wraper-->
+				</div><!--box-->
+			</div><!--flex-servicos-->
+		</div>
+	</section>
+
+	<section class="portfolio">
+		<div class="container">
+			<h2>Algumas empresas que tivemos a oportunidade de atuar.</h2>
+			<div class="empresas">
+				<div class="empresa-single">
+					<img src="images/bianchini.png" />
+				</div><!--empresa-single-->
+				<div class="empresa-single">
+					<img src="images/rancho.png" />
+				</div><!--empresa-single-->
+				<div class="empresa-single">
+					<img src="images/hoepcke.png" />
+				</div><!--empresa-single-->
+				<div class="empresa-single">
+					<img src="images/monguilhott.png" />
+				</div><!--empresa-single-->
+			</div><!--empresas-->
+		</div>
+	</section>
+
+	<section class="formulario">
+		<div class="container">
+			<h2>Um orçamento ou alguma dúvida?</h2>
+			<form>
+				<input type="text" name="nome" placeholder="Nome..." required>
+				<input type="text" name="email" placeholder="E-mail..." required>
+				<input type="text" name="telefone" placeholder="Telefone..." required>
+				<textarea name="mensagem" placeholder="Sua mensagem..."></textarea>
+				<input type="submit"  name="acao" value="Enviar!">
+			</form>
+		</div>
+	</section>
+
+	<footer>
+		<div class="container">
+			<div class="box-footer">
+				<h2>Endereço</h2>
+				<p>Rua Rafael Bandeira, 328 - Centro, Florianópolis/SC, 88015-450, Brasil</p>
+			</div>
+			<div class="box-footer">
+				<h2>Telefone</h2>
+				<p>(48)99901-3620</p>
+			</div>
+			<div class="box-footer">
+				<h2>Receba nossas news</h2>
+				<form>
+					<input placeholder="E-mail..." type="text" /><button type="submit"><i class="fa fa-paper-plane-o"></i></button>
+				</form>
+			</div>
+			<div class="clear"></div>
+		</div>
+	</footer>
+
+
+	<script src="js/jquery.js"></script>
+	<script>
+		$('nav.mobile h3').click(function(){
+			$('nav.mobile').find('ul').slideToggle();
+		})
+	</script>
+</body>
+</html>
